15a976a2ad2985fc2523255aed4fae3c05b44413
Theo Lima <theo.suporte@hotmail.com>
2022-01-14 16:39:04 -0300

    Implementa requisito 2

--- index.js
+++ index.js
@@ -14,6 +14,20 @@ app.get('/talker', (_request, response) => {
   response.status(HTTP_OK_STATUS).json(getTalker);
 });
 
+app.get('/talker/:talkerId', (req, res) => {
+  const { talkerId } = req.params;
+
+  const getTalker = JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
+
+  const getTalkerById = getTalker.find((talker) => talker.id === Number(talkerId));
+
+  if (!getTalkerById) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  return res.status(HTTP_OK_STATUS).json(getTalkerById);
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
