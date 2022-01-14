0b93380a2c0d0d3a1b130bbde31c25eaef850b90
Theo Lima <theo.suporte@hotmail.com>
2022-01-14 17:54:44 -0300

    Implementa o requisito 3

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+const RegExp = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/; // Gustavo Sant'Anna da turma 14B que forneceu o regex pra validar o email
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+// REQUISITO 1:
 
 app.get('/talker', (_request, response) => {
   const getTalker = JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
@@ -14,18 +17,47 @@ app.get('/talker', (_request, response) => {
   response.status(HTTP_OK_STATUS).json(getTalker);
 });
 
+// REQUISITO 2:
+
 app.get('/talker/:talkerId', (req, res) => {
   const { talkerId } = req.params;
 
   const getTalker = JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
 
-  const getTalkerById = getTalker.find((talker) => talker.id === Number(talkerId));
+  const getTalkerById = getTalker.find(
+    (talker) => talker.id === Number(talkerId),
+  );
 
   if (!getTalkerById) {
-    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    return res
+      .status(404)
+      .json({ message: 'Pessoa palestrante não encontrada' });
   }
 
   return res.status(HTTP_OK_STATUS).json(getTalkerById);
+});
+
+// REQUISITO 3:
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!RegExp.test(email)) { // função e regex feitas com ajuda de Gustavo San'Anna
+    return res
+      .status(400)
+      .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res
+      .status(400)
+      .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  return res.status(HTTP_OK_STATUS).json({ token: '7mqaVRXJSp886CGr' });
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
---
+++ login.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const HTTP_OK_STATUS = 200;
+const PORT = '3000';
+
+const 
+
+
+
+app.get('/', (_request, response) => {
+  response.status(HTTP_OK_STATUS).send();
+});
+
+app.listen(PORT, () => {
+  console.log('Online');
+});
