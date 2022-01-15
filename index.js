fc3d4ba335e1c4c65496eaf65463e16ba8ab392b
Theo Lima <theo.suporte@hotmail.com>
2022-01-15 03:01:49 -0300

    Implementa requisito 4

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
-// const {
-//   nameValidator,
-//   ageValidator,
-//   tokenValidator,
-// } = require('./validators/talkerValidator');
+const {
+  nameValidator,
+  tokenValidator,
+  ageValidator,
+  talkValidator,
+  rateValidator,
+  watchedValidator,
+} = require('./middlewares/talkerValidator');
+const { newToken } = require('./middlewares/generateToken');
 
 const app = express();
 app.use(bodyParser.json());
 
-const RegExp = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/; // Gustavo Sant'Anna da turma 14B que forneceu o regex pra validar o email
+const readTalkerFile = JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
+
+const RegExp = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/; //  Gustavo Sant'Anna da turma 14B que ajudou a fazer o regex pra validar o email
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 // REQUISITO 1:
 
 app.get('/talker', (_request, response) => {
-  const getTalker = JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
-
-  response.status(HTTP_OK_STATUS).json(getTalker);
+  response.status(HTTP_OK_STATUS).json(readTalkerFile);
 });
 
 // REQUISITO 2:
@@ -27,9 +31,7 @@ app.get('/talker', (_request, response) => {
 app.get('/talker/:talkerId', (req, res) => {
   const { talkerId } = req.params;
 
-  const getTalker = JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
-
-  const getTalkerById = getTalker.find(
+  const getTalkerById = readTalkerFile.find(
     (talker) => talker.id === Number(talkerId),
   );
 
@@ -49,32 +51,37 @@ app.post('/login', (req, res) => {
   if (!email) {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
-  if (!RegExp.test(email)) { // função e regex feitas com ajuda de Gustavo San'Anna
-    return res
-      .status(400)
-      .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  if (!RegExp.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   if (!password) {
-    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+    return res
+      .status(400).json({ message: 'O campo "password" é obrigatório' });
   }
   if (password.length < 6) {
-    return res
-      .status(400)
-      .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
-    }
-  return res.status(HTTP_OK_STATUS).json({ token: '7mqaVRXJSp886CGr' });
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+
+  return res.status(200).json({ token: `${newToken}` });
 });
 
 // REQUISITO 4:
 
-// app.post('/talker',
-//   nameValidator, 
-//   ageValidator,
-//   tokenValidator,
-//   (req, res) => {
-//   const { name, age, talk: { watchedAt, rate } } = req.body;
-//   return res.status(201).json({ name, age, talk: { watchedAt, rate } });
-// });
+app.post('/talker', 
+tokenValidator, nameValidator, ageValidator, 
+talkValidator, rateValidator, watchedValidator,
+(req, res) => {
+  const { name, age, talk: { watchedAt, rate } } = req.body;
+  const newTalker = {
+    name: `${name}`,
+    age,
+    id: readTalkerFile.length + 1,
+    talk: { rate, watchedAt: `${watchedAt}` },
+  };
+  readTalkerFile.push(newTalker);
+  fs.writeFileSync('./talker.json', JSON.stringify(readTalkerFile));
+  return res.status(201).json(newTalker);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
